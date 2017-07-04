227ed33d01a67d60a49f20c383a34991914a5489
NinaXu317 <c1xu@brandeis.edu>
2017-07-04 17:12:30 -0400

    commit

--- pages/users/client/users.js
+++ pages/users/client/users.js
@@ -9,6 +9,10 @@ Template.personrow.helpers({
 //   isOwner() {console.dir(this);
 //     return this.person.owner == Meteor.userId()}
 // })
+Template.addperson.onCreated(function bodyOnCreated() {
+  // this.state = new ReactiveDict();
+   Meteor.subscribe('people');
+});
 
 Template.addperson.events({
   'click button'(elt,instance) {
@@ -34,13 +38,13 @@ Template.addperson.events({
   }
 })
 
-Template.updatelocation.events({
-  'click button'(elt,instance) {
-    // console.dir(this);
-    // console.log(this.person._id);
-    Meteor.call('people.update',id,newinfo);
-  }
-})
+// Template.updatelocation.events({
+//   'click button'(elt,instance) {
+//     // console.dir(this);
+//     // console.log(this.person._id);
+//     Meteor.call('people.update',id,newinfo);
+//   }
+// })
 
 Template.personrow.events({
   'click button'(elt,instance) {
